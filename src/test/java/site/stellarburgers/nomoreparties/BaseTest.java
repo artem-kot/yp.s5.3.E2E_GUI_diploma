b085c09ef20a4a58a2ca370c61b33ea8a6836a24
artem-kot <artem.alex.kot@gmail.com>
2023-08-01 11:27:33 +0200

    Fixed CORS issue for Chrome 114.

--- src/test/java/site/stellarburgers/nomoreparties/BaseTest.java
+++ src/test/java/site/stellarburgers/nomoreparties/BaseTest.java
@@ -5,6 +5,9 @@ import io.qameta.allure.Step;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import site.stellarburgers.nomoreparties.rest.UserApi;
 import site.stellarburgers.nomoreparties.rest.UserPojo;
 
@@ -26,11 +29,12 @@ public class BaseTest {
     public void setUp() {
         /* Lines below are used to start Yandex.Browser */
 //        ChromeOptions options = new ChromeOptions();
-//        System.setProperty("webdriver.chrome.driver", "C:\\WebDriver\\bin\\yandexdriver.exe");
-//        options.setBinary("C:\\Users\\Banka\\AppData\\Local\\Yandex\\YandexBrowser\\Application\\browser.exe");
+//        System.setProperty("webdriver.chrome.driver", "PATH_TO yandexdriver.exe");
+//        options.setBinary("PATH_TO browser.exe");
 //        WebDriver driver = new ChromeDriver(options);
 
-        /* Lines below are used to start Yandex.Browser */
+        /* Lines below are used to start Chrome.Browser */
+        System.setProperty("chromeoptions.args", "--remote-allow-origins=*");
         WebDriverManager.chromedriver().setup();
         browser = "chrome";
         driverManagerEnabled = true;
